9a6b0cb93fcb51a386fa05b355f0915d780aa8e4
Davy <davy.marthely@ynov.com>
2023-02-07 08:50:46 +0100

    Ebauche premier troll

---
+++ Script/redirection.js
@@ -0,0 +1,53 @@
+const fake = document.querySelector('.modal-btn')
+const hide = document.querySelector('.wrapper')
+const modal = document.querySelector('.modal')
+const prank = document.querySelector('.prank')
+const body = document.querySelector('body')
+
+document.addEventListener("DOMContentLoaded", () => {
+fake.addEventListener("click", () => {
+    prank.style.visibility="visible"
+    hide.style.display="none"
+    modal.style.display="none"
+    body.style.cursor="none"
+    toggleFullScreen()
+})
+})
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else if (document.exitFullscreen) {
+        document.exitFullscreen();
+    }
+}
+//
+// for(i=0; i<100; i++) {
+//     // Random rotation
+//     var randomRotation = Math.floor(Math.random() * 360);
+//     // Random Scale
+//     var randomScale = Math.random() * 1;
+//     // Random width & height between 0 and viewport
+//     var randomWidth = Math.floor(Math.random() * Math.max(document.documentElement.clientWidth, window.innerWidth || 0));
+//     var randomHeight =  Math.floor(Math.random() * Math.max(document.documentElement.clientHeight, window.innerHeight || 500));
+//
+//     // Random animation-delay
+//     var randomAnimationDelay = Math.floor(Math.random() * 15);
+//     console.log(randomAnimationDelay);
+//
+//     // Random colors
+//     var colors = ['#0CD977', '#FF1C1C', '#FF93DE', '#5767ED', '#FFC61C', '#8497B0']
+//     var randomColor = colors[Math.floor(Math.random() * colors.length)];
+//
+//     // Create confetti piece
+//     var confetti = document.createElement('div');
+//     confetti.className = 'confetti';
+//     confetti.style.top=randomHeight + 'px';
+//     confetti.style.right=randomWidth + 'px';
+//     confetti.style.backgroundColor=randomColor;
+//     // confetti.style.transform='scale(' + randomScale + ')';
+//     confetti.style.obacity=randomScale;
+//     confetti.style.transform='skew(15deg) rotate(' + randomRotation + 'deg)';
+//     confetti.style.animationDelay=randomAnimationDelay + 's';
+//     document.getElementById("confetti-wrapper").appendChild(confetti);
+// }
